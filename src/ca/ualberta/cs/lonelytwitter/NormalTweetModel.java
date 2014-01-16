0672fb5faa346eb2a0157eb3e53374ad1a0dd32a
ssowemim <ssowemim@ualberta.ca>
2014-01-16 15:58:08 -0700

    Normal class was also made

---
+++ src/ca/ualberta/cs/lonelytwitter/NormalTweetModel.java
@@ -0,0 +1,32 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+
+public class NormalTweetModel extends LonleyTweetModel
+{
+
+	public NormalTweetModel(String text)
+	{
+
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Date getTimestamp()
+	{
+
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTypeTweet()
+	{
+
+		// TODO Auto-generated method stub
+		return "Normal Tweet!";
+	}
+
+}
