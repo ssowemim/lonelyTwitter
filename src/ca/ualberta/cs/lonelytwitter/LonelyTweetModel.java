4fd5fe257c990ff8b79ee09a2b9c682963478361
ssowemim <ssowemim@ualberta.ca>
2014-01-23 20:25:39 -0700

    Added lonely tweet model!

---
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
@@ -0,0 +1,42 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+
+public abstract class LonelyTweetModel{
+        protected String text;
+        protected Date timestamp;
+
+        public String getText(){
+        
+                return text;
+        }
+        
+        public abstract void setText(String text);
+        
+        public Date getTimestamp(){
+        
+                return timestamp;
+        }
+        
+        public void setTimestamp(Date timestamp){
+        
+                this.timestamp = timestamp;
+        }
+        
+        public LonelyTweetModel(String text, Date timestamp){
+                super();
+                this.text = text;
+                this.timestamp = timestamp;
+        }
+        
+        public LonelyTweetModel(String text){
+                super();
+                this.text = text;
+                this.timestamp = new Date();
+        }
+        
+        public abstract boolean isNormal();
+        
+        public abstract void setImportant(boolean important) throws Exception;
+}
